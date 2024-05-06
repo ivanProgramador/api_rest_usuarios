0271b9623236e6b7973688ed4966745b06dc3bb5
ivan <ivan-guita@hotmail.com>
2024-05-06 09:37:07 -0300

    primeira rota criada

---
+++ controllers/anotaçoes/anotacoes.js
@@ -0,0 +1,10 @@
+/*
+Como esse projeto não tem inteface grafica todos os processos vão ser executados,
+usando rotas.
+
+Rota 1 criada, essa rota vai servir para fazer oi cadastro de novos ususarios no sistema
+ 
+router.post('/user',UserController.create); 
+
+
+*/
