5853840648f81452c76f6d3df4ad2b0f9c188c90
ivan <ivan-guita@hotmail.com>
2024-05-06 09:18:34 -0300

    Banco de dadose tabela  criados / conexão feita

---
+++ database/anotacoes/sobre_tabela.js
@@ -0,0 +1,13 @@
+/*
+
+
+Esses são dados vbasicos da tabela deste projeto
+
+id autocrement primary key
+name varchar 50   -  permite dados nulos
+email varchar 100 -  não permite nulos porque eu vou precisar usar ele como login 
+senha varchar 100 -  vou precisar desse tamaenho porque ela vai armazenada como hash 
+role  var char 3  -  essa coluna diferencia ussuarios comensa dos administradores     
+
+
+*/
